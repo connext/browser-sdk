ec02c71a0a1d967926fe6fcd035be706e94dbd5f
Daniel Lee <djklee@edu.uwaterloo.ca>
2020-08-14 17:51:34 -0400

    Add demo app from create-react-app

---
+++ demo-app/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  const handleClick = () => {}
+  return (
+    <div className="App">
+      <div className="Content">
+        <h4>(-⌣-) Welcome to Loft Rad</h4>
+        <h1>Golden</h1>
+        <h3>Tom Doolie</h3>
+        <button onClick={handleClick}>
+          Tip
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
