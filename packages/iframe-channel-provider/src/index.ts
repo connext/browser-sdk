5e08230abb2cb569d7276a2f04fc0249ba85e7b3
Pedro Gomes <pedrogomes94@gmail.com>
2020-08-14 17:02:29 +0200

    iframe-channel-provider

--- packages/iframe-channel-provider/src/index.ts
+++ packages/iframe-channel-provider/src/index.ts
@@ -1,3 +1,13 @@
-class IframeChannelProvider {}
+import { ChannelProvider } from "@connext/channel-provider";
+import { IRpcConnection } from "@connext/types";
+
+import IframeRpcConnection from "./rpc";
+
+class IframeChannelProvider extends ChannelProvider {
+  constructor() {
+    super(new IframeRpcConnection() as IRpcConnection);
+  }
+  public isIframe = true;
+}
 
 export default IframeChannelProvider;
