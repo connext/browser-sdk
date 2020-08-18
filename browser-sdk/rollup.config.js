9a706c73f87011d4d8e43cad1479de3465b9c10e
Pedro Gomes <pedrogomes94@gmail.com>
2020-08-18 17:14:13 +0200

    add channel provider

--- browser-sdk/rollup.config.js
+++ browser-sdk/rollup.config.js
@@ -1,15 +1,20 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
+import json from "@rollup/plugin-json";
 import replace from "@rollup/plugin-replace";
+import typescript from "rollup-plugin-typescript2";
 import pkg from "./package.json";
 
 export default {
   input: "./src/index.tsx",
   output: [{ file: pkg.main, format: "umd", name: "ConnextSDK" }],
   plugins: [
-    nodeResolve(), // resolve module names using node_modules and the standard Node module resolution algorithm
+    nodeResolve({
+      preferBuiltins: true,
+      modulesOnly: true,
+    }), // resolve module names using node_modules and the standard Node module resolution algorithm
     commonjs(), // compile the module to CommonJS (required in order to support the UMD standard)
+    json(),
     replace({ "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV) }), // substitute the "NODE_ENV" environment variable for its real value within the compiled output (since environment variables are unavailable in the browser)
     typescript(), // compile Typescript to ES5 as per tsconfig.json
   ],
