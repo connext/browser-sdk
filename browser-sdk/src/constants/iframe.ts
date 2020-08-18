fb2acd2cf3251f5f071f4843c9d3332f2b03e8d5
Anthony Zhang <me@anthonyz.ca>
2020-08-17 23:22:28 -0400

    Working SDK and iframe communication via sequenced postmessage protocol

--- browser-sdk/src/constants/iframe.ts
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_IFRAME_SRC = "http://localhost:3030";
-export const DEFAULT_IFRAME_ID = "channel-provider-iframe";
--- browser-sdk/src/helpers/document.ts
+++
@@ -1,8 +0,0 @@
-export function renderElement(name: string, attr: any, target = "body") {
-  const elm = document.createElement(name);
-  Object.keys(attr).forEach((key) => {
-    elm[key] = attr[key];
-  });
-  document[target].appendChild(elm);
-  return elm;
-}
