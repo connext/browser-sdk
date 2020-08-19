535bfbb01c23abc95457aaaa545ad936c9f41884
Pedro Gomes <pedrogomes94@gmail.com>
2020-08-19 20:39:06 +0200

    add withdraw and refactor emit events

---
+++ browser-sdk/src/constants/events.ts
@@ -0,0 +1,2 @@
+export const LOGIN_SUCCESS_EVENT = "login_success";
+export const WITHDRAW_SUCCESS_EVENT = "withdraw_success";
---
+++ browser-sdk/src/helpers/ethereum.ts
@@ -0,0 +1,18 @@
+import { utils } from "ethers";
+
+export const isValidAddress = (address?: string) => {
+  if (!address) {
+    return false;
+  } else if (address.toLowerCase().substring(0, 2) !== "0x") {
+    return false;
+  } else if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
+    return false;
+  } else if (
+    /^(0x)?[0-9a-f]{40}$/.test(address) ||
+    /^(0x)?[0-9A-F]{40}$/.test(address)
+  ) {
+    return true;
+  } else {
+    return address === utils.getAddress(address);
+  }
+};
