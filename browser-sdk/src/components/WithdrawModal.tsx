ef9ca08e46e66025dfa3002ac5e2d8d636c82b45
Daniel Lee <djklee@edu.uwaterloo.ca>
2020-08-21 12:34:27 -0400

    Get withdraw modal ref from event

--- browser-sdk/src/components/WithdrawModal.tsx
+++ browser-sdk/src/components/WithdrawModal.tsx
@@ -5,14 +5,13 @@ import { isValidAddress } from "../helpers";
 function WithdrawModal({ sdkInstance, onWithdrawComplete }) {
   const [recipient, setRecipient] = useState('');
   const [amount, setAmount] = useState('');
-  const recipientRef = useRef<HTMLInputElement>(null);
 
   const withdraw = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (!isValidAddress(recipient)) {
       console.log("Invalid address!");
-      recipientRef.current?.setCustomValidity("Please enter a valid address");
+      (e.currentTarget.elements.namedItem("recipient") as HTMLInputElement)?.setCustomValidity("Please enter a valid address");
       return;
     }
 
@@ -42,7 +41,7 @@ function WithdrawModal({ sdkInstance, onWithdrawComplete }) {
       <form onSubmit={withdraw}>
         <h3>Please enter amount to withdraw and recipient.</h3>
         <input required type="number" placeholder="Amount" value={amount} step="any"  onChange={e => setAmount(e.target.value)} />
-        <input required ref={recipientRef} placeholder="Ethereum address" value={recipient} onChange={e => setRecipient(e.target.value)} />
+        <input required name="recipient" placeholder="Ethereum address" value={recipient} onChange={e => setRecipient(e.target.value)} />
         <button type="submit">Withdraw</button>
       </form>
     </div>
