959c72642624c777a828531e3fc20251d2e46afc
Daniel Lee <djklee@edu.uwaterloo.ca>
2020-08-21 13:39:48 -0400

    Make demo app more detailed

--- demo-app/src/App.tsx
+++ demo-app/src/App.tsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import ConnextSDK from "@connext/browser-sdk";
 
 function App() {
+  const [tipped, setTipped] = useState(false);
+  const [errored, setErrored] = useState(false);
+
+  const recipientIdentifier = "indra987zxy...";
+  const amount = 0.01;
+
   const connext = new ConnextSDK();
+
   const handleClick = async () => {
     await connext.login();
-    await connext.deposit();
+    const currentBalance = await connext.balance();
+    if (currentBalance < amount) {
+      console.log("Balance too low! Please fund your account.");
+      try {
+        await connext.deposit();
+      } catch (error) {
+        setErrored(true);
+        console.log(error);
+      }
+    } else {
+      try {
+        await connext.transfer(recipientIdentifier, amount);
+        setTipped(true);
+        setErrored(false);
+      } catch (error) {
+        setErrored(true);
+        console.log(error);
+      }
+    }
   };
   return (
     <div className="App">
@@ -14,7 +39,7 @@ function App() {
         <h4>(-⌣-) Welcome to Loft Rad</h4>
         <h1>Golden</h1>
         <h3>Tom Doolie</h3>
-        <button onClick={handleClick}>Tip</button>
+        <button onClick={handleClick}>{tipped ? "Thanks for the tip!" : errored ? "Error! Please try again" : "Tip"}</button>
       </div>
     </div>
   );
