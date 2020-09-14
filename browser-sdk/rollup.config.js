ced99d334078737de2f80fdeded87b692142997a
Anthony Zhang <me@anthonyz.ca>
2020-09-14 19:26:08 -0400

    Upgrade to the latest Connext SDK version

--- browser-sdk/rollup.config.js
+++ browser-sdk/rollup.config.js
@@ -24,29 +24,14 @@ export default {
       // however, the polyfills we're using don't support the randomBytes function, so we'll implement our own shim based on the WebCrypto API
       'crypto.randomBytes': '(function(l){return window.crypto.getRandomValues(new Uint8Array(l));})',
 
-      // fix some unusual imports in the "memoizee" and "d" NPM modules - they import the "#" module, which is technically an invalid module name - this makes rollup throw an error, where a lazier packer might just let it slide
-      // we'll just re-implement them since they're pretty trivial functions
-      'require("es5-ext/array/#/e-index-of")': `function(searchElement = undefined, fromIndex = undefined) {
-        if (!Number.isNaN(searchElement)) return this.indexOf(searchElement, fromIndex);
-        var newFromIndex = Number.isNaN(fromIndex) ? 0 : fromIndex >= 0 ? Math.floor(fromIndex) : this.length - Math.floor(Math.abs(fromIndex));
-        for (var i = newFromIndex; i < this.length; ++i) {
-          if (this.hasOwnProperty(i) && Number.isNaN(this[i])) return i;
-        }
-        return -1;
-      }`,
-      'require("es5-ext/string/#/contains")': 'function(s, p) { return String.prototype.indexOf.call(this, s, p) > -1; }',
-      delimiters: ['', ''],
+      // fix "jsonwebtoken" library using "process.version" for some reason, which makes the whole thing fail in the browser when it turns into "browser.version"
+      "browser.version": '"8.0.0"',
     }),
     nodeResolve({browser: true, preferBuiltins: false}), // resolve module names using node_modules and the standard Node module resolution algorithm
     commonjs({requireReturnsDefault: "preferred"}), // compile the module to CommonJS (required in order to support the UMD standard)
     nodePolyfills(),  // add shims for node.js builtin modules such as "process" and "os"
     json(),  // support importing JSON files as if they are modules
     typescript(), // compile Typescript to ES5 as per tsconfig.json
-    replace({
-      // the "memoizee" library attempts to perform illegal operations such as modifying modules imported via require() - this causes issues because the generated code tries to prevent this behaviour using Object.freeze
-      // as a hack, we'll disable freezing the object in order to allow this illegal behaviour in "memoizee"
-      'Object.freeze': '(function(x){return x;})',
-    }),
   ],
   context: "window", // set the value of `this` to `window` at the top-level - Magic Link's SDK depends on this behaviour
 };
